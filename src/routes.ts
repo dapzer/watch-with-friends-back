d84a1235c0ce2dd421822e854c77385d0af593bb
Danila Voronkov <danilavoronkov2002@gmail.com>
2023-03-11 15:12:31 +0300

    feat(routes): Init first route

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { FastifyInstance } from 'fastify';
 
 export const websocketsRoutes = async (server: FastifyInstance) => {
-  server.get('/', (request, reply) => ({ msg: 'e' }));
+  server.get('/', (request, reply) => {
+    reply.send({ msg: 'Eboy' });
+  });
 };
