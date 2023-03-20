0e2132af8f084f7ad5e1527c75c257d73df7eeb0
Danila Voronkov <danilavoronkov2002@gmail.com>
2023-03-20 14:27:20 +0300

    feat(api): Allow cors for localhost

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Fastify from 'fastify';
 import ws from '@fastify/websocket';
-
+import cors from '@fastify/cors';
 // @ts-ignore
 import { websocketsRoutes } from './routes/websockets.ts';
 // @ts-ignore
@@ -10,16 +10,22 @@ const port = 8080;
 
 export const fastify = Fastify({});
 fastify.register(ws);
+await fastify.register(cors, {
+  delegator: (req, callback) => {
+    const corsOptions = {
+      origin: true,
+    };
+
+    if (/^localhost$/m.test(req.headers.origin!)) {
+      corsOptions.origin = false;
+    }
+
+    callback(null, corsOptions);
+  },
+});
 
 fastify.register(websocketsRoutes, { prefix: 'api/ws' });
 fastify.register(roomsRouter, { prefix: 'api/rooms' });
-
-export function broadcast(message: string): void {
-  // eslint-disable-next-line no-restricted-syntax
-  for (const client of fastify.websocketServer.clients) {
-    client.send(message);
-  }
-}
 
 fastify.listen({ port }, (err, address) => {
   if (err) {
